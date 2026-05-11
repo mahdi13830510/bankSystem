79b561237ecef447a20fbccc1e8e43475b7cb21f
Nima <nimars1394sh@gmail.com>
2026-05-11 11:33:00 +0330

    add: authenticatioAapp services  added

---
+++ apps/authentication/services.py
@@ -0,0 +1,95 @@
+import jwt
+import random
+
+from django.conf import settings
+from django.utils import timezone
+from datetime import timedelta
+
+from apps.users.models import User
+from .models import Session, OTPCode
+
+
+class AuthService:
+
+    @staticmethod
+    def login(phone, password, ip, device):
+
+        user = User.objects.filter(phone=phone).first()
+
+        if not user:
+            raise Exception("User not found")
+
+        if user.is_blocked:
+            raise Exception("User blocked")
+
+        if not user.check_password(password):
+            user.failed_login_attempts += 1
+            user.save()
+            raise Exception("Wrong password")
+
+        user.failed_login_attempts = 0
+        user.save()
+
+        code = str(random.randint(100000, 999999))
+
+        OTPCode.objects.create(
+            user=user,
+            code=code,
+            expires_at=timezone.now() + timedelta(minutes=2)
+        )
+
+        return user
+
+    @staticmethod
+    def verify_otp(phone, code, ip, device):
+
+        user = User.objects.get(phone=phone)
+
+        otp = OTPCode.objects.filter(
+            user=user,
+            code=code,
+            is_used=False
+        ).last()
+
+        if not otp:
+            raise Exception("Invalid OTP")
+
+        if otp.expires_at < timezone.now():
+            raise Exception("OTP expired")
+
+        otp.is_used = True
+        otp.save()
+
+        access = jwt.encode(
+            {"user_id": user.id},
+            settings.SECRET_KEY,
+            algorithm="HS256"
+        )
+
+        refresh = jwt.encode(
+            {"user_id": user.id, "type": "refresh"},
+            settings.SECRET_KEY,
+            algorithm="HS256"
+        )
+
+        Session.objects.create(
+            user=user,
+            access_token=access,
+            refresh_token=refresh,
+            device_name=device,
+            ip_address=ip,
+            expires_at=timezone.now() + timedelta(days=7)
+        )
+
+        return access, refresh
+
+    @staticmethod
+    def logout(refresh_token):
+
+        session = Session.objects.filter(
+            refresh_token=refresh_token
+        ).first()
+
+        if session:
+            session.status = "revoked"
+            session.save()
