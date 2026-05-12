239fa95d8000a413231bc7bcf587fb5976e0db74
Nima <nimars1394sh@gmail.com>
2026-05-12 18:48:00 +0330

    fix: accountsApp permissions fixed

--- apps/accounts/permissions.py
+++ apps/accounts/permissions.py
@@ -3,4 +3,4 @@ from rest_framework.permissions import BasePermission
 
 class IsBankStaff(BasePermission):
     def has_permission(self, request, view):
-        return request.user.is_staff
+        return request.user.is_staff or request.user.is_superuser
