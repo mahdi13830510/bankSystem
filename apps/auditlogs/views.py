e808a6fc9624597874bc1d0ea47739757f53f48c
Nima <nimars1394sh@gmail.com>
2026-05-13 01:39:00 +0330

    add: auditlogApp views  added

--- apps/auditlogs/views.py
+++ apps/auditlogs/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import AuditLog
 
-# Create your views here.
+
+class AuditLogListView(APIView):
+
+    def get(self, request):
+
+        logs = AuditLog.objects.all()[:100]
+
+        return Response([
+            {
+                "action": l.action,
+                "entity_type": l.entity_type,
+                "entity_id": l.entity_id,
+                "created_at": l.created_at
+            }
+            for l in logs
+        ])
