4654a9dd10a3a69b21878e1fbca16cf0de8d3b9d
Nima <nimars1394sh@gmail.com>
2026-05-12 23:48:00 +0330

    add: fraudApp moudle  added

--- apps/fraud/models.py
+++ apps/fraud/models.py
@@ -1,3 +1,30 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+
+class FraudDecision(models.TextChoices):
+    SAFE = "SAFE"
+    SUSPICIOUS = "SUSPICIOUS"
+    BLOCKED = "BLOCKED"
+
+
+class FraudReport(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+    transaction_id = models.UUIDField()
+
+    user_id = models.UUIDField()
+
+    score = models.IntegerField()  # 0-100
+
+    decision = models.CharField(
+        max_length=20,
+        choices=FraudDecision.choices
+    )
+
+    reason = models.JSONField(default=dict)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = "fraud_reports"
