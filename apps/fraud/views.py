6683dada86dbad4bf2c704f6d005f55432dde58d
Nima <nimars1394sh@gmail.com>
2026-05-13 00:27:00 +0330

    add: fraudApp views  added

--- apps/fraud/views.py
+++ apps/fraud/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import FraudReport
 
-# Create your views here.
+
+class FraudReportListView(APIView):
+
+    def get(self, request):
+        reports = FraudReport.objects.all().order_by("-created_at")
+
+        return Response([
+            {
+                "id": r.id,
+                "score": r.score,
+                "decision": r.decision,
+                "transaction_id": r.transaction_id
+            }
+            for r in reports
+        ])
