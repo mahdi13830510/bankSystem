b8b47916a9caf3380fa2fa34809ee3651c20eadd
Nima <nimars1394sh@gmail.com>
2026-05-13 00:49:00 +0330

    add: auditlogApp module  added

--- apps/auditlogs/models.py
+++ apps/auditlogs/models.py
@@ -1,3 +1,37 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+
+class AuditActionType(models.TextChoices):
+    LOGIN = "LOGIN"
+    LOGOUT = "LOGOUT"
+    TRANSACTION = "TRANSACTION"
+    FRAUD_DECISION = "FRAUD_DECISION"
+    LOAN = "LOAN"
+    ACCOUNT = "ACCOUNT"
+    ADMIN = "ADMIN"
+    SYSTEM = "SYSTEM"
+
+
+class AuditLog(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+    user_id = models.UUIDField(null=True, blank=True)
+
+    action = models.CharField(max_length=30, choices=AuditActionType.choices)
+
+    entity_type = models.CharField(max_length=50)   # e.g. Transaction, Account
+
+    entity_id = models.CharField(max_length=100)
+
+    ip_address = models.GenericIPAddressField(null=True, blank=True)
+
+    user_agent = models.TextField(blank=True, null=True)
+
+    metadata = models.JSONField(default=dict)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = "audit_logs"
+        ordering = ["-created_at"]
