104255bc3c4570b69c708145a5f8fe9796b3d1a0
Nima <nimars1394sh@gmail.com>
2026-05-14 13:20:00 -0500

    add:  banksApp views added

--- apps/banks/views.py
+++ apps/banks/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+from rest_framework.generics import (
+    ListAPIView,
+    RetrieveAPIView,
+    CreateAPIView,
+    UpdateAPIView
+)
+from rest_framework.permissions import IsAdminUser
 
-# Create your views here.
+from .models import Bank, Branch
+from .serializers import (
+    BankSerializer,
+    BranchSerializer
+)
+
+
+class BankListView(ListAPIView):
+    queryset = Bank.objects.filter(status="ACTIVE")
+    serializer_class = BankSerializer
+
+
+class BankDetailView(RetrieveAPIView):
+    queryset = Bank.objects.all()
+    serializer_class = BankSerializer
+
+
+class BankCreateView(CreateAPIView):
+    queryset = Bank.objects.all()
+    serializer_class = BankSerializer
+    permission_classes = [IsAdminUser]
+
+
+class BankStatusUpdateView(UpdateAPIView):
+    queryset = Bank.objects.all()
+    serializer_class = BankSerializer
+    permission_classes = [IsAdminUser]
+
+
+class BranchListView(ListAPIView):
+    serializer_class = BranchSerializer
+
+    def get_queryset(self):
+        return Branch.objects.filter(
+            bank_id=self.kwargs["bank_id"],
+            is_active=True
+        )
