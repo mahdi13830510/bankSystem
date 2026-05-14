c90f33bfeccc884f652a70e9d214d9a2e6503dfe
Nima <nimars1394sh@gmail.com>
2026-05-14 18:28:00 -0500

    add:  installment app serializers added

---
+++ apps/installments/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import Installment
+
+
+class InstallmentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Installment
+        fields = "__all__"
