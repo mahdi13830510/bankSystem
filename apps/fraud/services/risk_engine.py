d266e78c43d8a49b59781e85978edb78c94d435e
Nima <nimars1394sh@gmail.com>
2026-05-14 12:47:00 -0500

    feat:  banksapp integrated with fraud

--- apps/fraud/services/risk_engine.py
+++ apps/fraud/services/risk_engine.py
@@ -1,22 +1,42 @@
+from apps.banks.models import BankStatus
+
+
 class RiskEngine:
 
     @staticmethod
-    def calculate_score(*, user, amount, ip, history_count):
+    def calculate_score(
+        *,
+        transaction,
+        ip,
+        history_count
+    ):
 
         score = 0
 
-        # amount anomaly
+        amount = transaction.amount
+        source_bank = transaction.account.bank
+        destination_bank = transaction.destination_account.bank
+
+        # amount risk
         if amount > 5000:
             score += 30
+
         if amount > 10000:
+            score += 20
+
+        # velocity
+        if history_count > 5:
+            score += 20
+
+        # destination bank risk
+        if destination_bank.status == BankStatus.SUSPENDED:
             score += 50
 
-        # velocity check
-        if history_count > 5:
+        elif destination_bank.status == BankStatus.MAINTENANCE:
             score += 25
 
-        # new device / ip risk
-        if ip.startswith("10."):
+        # interbank risk
+        if source_bank.id != destination_bank.id:
             score += 10
 
         return min(score, 100)
