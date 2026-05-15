9a2fd0b1904c045bc47f564f5b934c5f6463a5ca
Nima <nimars1394sh@gmail.com>
2026-05-14 22:16:00 -0500

    add:  notifications app views added

--- apps/notifications/views.py
+++ apps/notifications/views.py
@@ -1,3 +1,46 @@
-from django.shortcuts import render
+from rest_framework.generics import ListAPIView
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
-# Create your views here.
+from .models import Notification
+from .serializers import NotificationSerializer
+from .services import NotificationService
+
+
+class MyNotificationsView(ListAPIView):
+    serializer_class = NotificationSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Notification.objects.filter(
+            user=self.request.user
+        )
+
+
+class MarkAsReadView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, pk):
+
+        obj = Notification.objects.get(
+            pk=pk,
+            user=request.user
+        )
+
+        NotificationService.mark_read(obj)
+
+        return Response({"detail": "read"})
+
+
+class UnreadCountView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+
+        count = Notification.objects.filter(
+            user=request.user,
+            is_read=False
+        ).count()
+
+        return Response({"unread": count})
