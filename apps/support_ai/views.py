60da3d5efffc7e85761dcd0e960a49d13de88fae
Nima <nimars1394sh@gmail.com>
2026-05-15 08:19:00 -0500

    add: support ai views added

--- apps/support_ai/views.py
+++ apps/support_ai/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
-# Create your views here.
+from .services import AIService
+from .models import AIConversation
+from .serializers import AIConversationSerializer
+
+
+class ChatWithAIView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+
+        message = request.data.get("message")
+
+        response = AIService.send_message(
+            request.user,
+            message
+        )
+
+        return Response({"response": response})
+
+
+class ConversationView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+
+        conv = AIConversation.objects.filter(user=request.user).first()
+
+        return Response(
+            AIConversationSerializer(conv).data
+        )
