07daf4d04fe3ff2b9035af71b60309da6b0f918c
mahdi dolati <mahdi.dlt13830510@gmail.com>
2026-06-10 11:07:00 +0330

    fix: replace simplejwt auth backend with custom JWTBearerAuthentication

--- banksystem/settings.py
+++ banksystem/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
-        "rest_framework_simplejwt.authentication.JWTAuthentication",
+        "apps.authentication.backends.JWTBearerAuthentication",
     ),
 }
 
